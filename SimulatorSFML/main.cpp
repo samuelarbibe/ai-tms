cab9933d9c65ef12f60f16ff233cd6a35d06ca7c
Samuel Arbibe <38098325+samuelarbibe@users.noreply.github.com>
2019-11-18 11:19:05 +0200

    Initial Commit

---
+++ SimulatorSFML/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimulatorSFML
+//
+//  Created by Samuel Arbibe on 18/11/2019.
+//  Copyright © 2019 Samuel Arbibe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
