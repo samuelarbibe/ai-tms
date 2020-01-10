de27f6969d32304384e824af1146b405ab551aa5
Samuel Arbibe <38098325+samuelarbibe@users.noreply.github.com>
2020-01-10 16:36:13 +0200

    UI Progress 🎮

--- src/UI/mainwindow.cpp
+++ src/UI/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    SimulatorEngine = new Engine(ui->frame);
+    SimulatorEngine = new Engine(ui->SimulatorFrame);
 }
 
 MainWindow::~MainWindow()
